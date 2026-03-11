5d611f1a59932108d29ea10a0309f1737bd9514b
raguram-muthukrishnan <2201721033039@mcc.edu.in>
2026-03-11 19:02:01 +0530

    fix 4 - tesing step 1

--- app/root.tsx
+++ app/root.tsx
@@ -140,7 +140,7 @@ export async function loader(args: Route.LoaderArgs) {
     consent: {
       checkoutDomain: env.PUBLIC_CHECKOUT_DOMAIN!,
       storefrontAccessToken: env.PUBLIC_STOREFRONT_API_TOKEN!,
-      withPrivacyBanner: true,
+      withPrivacyBanner: false, // STEP 1 DEBUG: disabled to test if consent banner is blocking analytics
       // localize the privacy banner
       country: args.context.storefront.i18n.country,
       language: args.context.storefront.i18n.language,
